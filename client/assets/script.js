cfb43a70aab3a9e34f0e9873a14aa3fa5302625c
un5450 <un5450@outlook.com>
2023-11-01 09:50:51 +0900

    more work on comments

--- client/assets/script.js
+++ client/assets/script.js
@@ -1,6 +1,6 @@
 
 // [DEBUG] placeholder replacement
-addEventListener("load", () => {
+addEventListener("DOMContentLoaded", () => {
     console.log("[DEBUG] Executing client-side placeholder replacement.")
 
     const replacements = {
@@ -18,18 +18,32 @@ addEventListener("load", () => {
     }
 
     const placeholderRegex = /\{\{#([a-zA-Z0-9_.]+)\}\}/g
-    const innerHtml = document.documentElement.innerHTML
-    document.documentElement.innerHTML = innerHtml.replaceAll(placeholderRegex, (match, p1) => {
-        return replacements[p1] || match
-    })
-    document.documentElement.lang = document.documentElement.lang.replaceAll(placeholderRegex, (match, p1) => {
-        return replacements[p1] || match
-    })
+    const replaceAll = (txt) => txt.replaceAll(placeholderRegex, (orig, p1) => replacements[p1] || orig)
+
+    document.documentElement.lang = replaceAll(document.body.lang)
+
+    const walker = document.createTreeWalker(
+        document.documentElement,
+        NodeFilter.SHOW_TEXT,
+        node => !!node.data?.trim()
+    )
+    const textNodes = []
+    while (walker.nextNode()) {textNodes.push(walker.currentNode)}
+
+    for (node of textNodes) {
+        const newData = replaceAll(node.data)
+        if (node.data != newData) {
+            const template = document.createElement("template")
+            template.innerHTML = newData
+            node.parentNode.insertBefore(template.content, node)
+            node.parentNode.removeChild(node)
+        }
+    }
 })
 
 
 // Add comment
-addEventListener("load", () => {
+addEventListener("DOMContentLoaded", () => {
     const addCommentButton = document.querySelectorAll("article button.add-comment")
     if (!addCommentButton) { return }
 
@@ -39,6 +53,15 @@ addEventListener("load", () => {
             const target = ev.target
             
             document.createElement()
+        })
+    })
+})
+
+addEventListener("DOMContentLoaded", () => {
+    const textareas = document.querySelectorAll("textarea[data-autosize]")
+    textareas.forEach((elem) => {
+        elem.addEventListener("input", (ev) => {
+            ev.target.style.height = ev.target.scrollHeight + "px"
         })
     })
 })
